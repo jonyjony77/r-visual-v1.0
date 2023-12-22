5604ba0e3d03f7fe78a00d77d6c581148c56e05e
jonyjony77 <73456802+jonyjony77@users.noreply.github.com>
2023-12-22 12:57:12 +0800

    Add files via upload

---
+++ r-visual/fxmanifest.lua
@@ -0,0 +1,78 @@
+fx_version 'cerulean'
+game 'gta5'
+
+author 'RW-DEVELOPER'
+description 'ServerSide Graphics'
+
+files {
+	'stream/breakableglass.ytd',
+    'stream/cloudhat_clear01.ytd',
+    'stream/cloudhat_cloudy.ytd',
+    'stream/cloudhat_contrails.ytd',
+    'stream/cloudhat_fog.ydr',
+    'stream/cloudhat_horizon.ytd',
+    'stream/cloudhat_nimbus.ytd',
+    'stream/cloudhat_wispy.ytd',
+    'stream/explosionfx.dat',
+    'stream/firefx.dat',
+    'stream/frontend.ytd',
+    'stream/fxdecal.ytd',
+    'stream/fxweather.ytd',
+    'stream/graphics.ytd',
+    'stream/graphics_pc.ytd',
+    'stream/gta5_cache_y.dat',
+    'stream/skydome.ydd',
+    'stream/skydome.ytd',
+    'stream/water.ytd',
+
+
+	'data/hbaosettings.xml',
+    'data/visualsettings.dat',
+    'data/cloudkeyframes.xml',
+    'data/clouds.xml',
+    'data/cpqsmix_ssaosettings.xml',
+    'data/lensflare_f.xml',
+    'data/lensflare_t.xml',
+    'data/lensflare_m.xml',
+    'data/lensflare_t.xml',
+    'data/watertune.xml',
+    'data/effects/vfxlightningsettings.xml',
+    'data/effects/wheelfx.dat',
+    'data/glass/glassconfig.xml',
+    'data/levels/gta5/desert_emitter_ground.xml',
+    'data/levels/gta5/desert_emitter_ground_heavy.xml',
+    'data/levels/gta5/desert_render_ground.xml',
+    'data/levels/gta5/desert_render_ground_heavy.xml',
+    'data/levels/gta5/blizzard_emitter_drop.xml',
+    'data/levels/gta5/blizzard_emitter_ground.xml',
+    'data/levels/gta5/blizzard_emitter_mist.xml',
+    'data/levels/gta5/blizzard_render_drop.xml',
+    'data/levels/gta5/blizzard_render_mist.xml',
+    'data/levels/gta5/firefly_render_drop.xml',
+    'data/levels/gta5/fly_render_drop.xml',
+    'data/levels/gta5/fog_render_ground.xml',
+    'data/levels/gta5/lake_fog_render_ground.xml',
+    'data/levels/gta5/pollen_render_drop.xml',
+    'data/levels/gta5/snowheavy_emitter_drop.xml',
+    'data/levels/gta5/snowheavy_emitter_ground.xml',
+    'data/levels/gta5/snowheavy_emitter_mist.xml',
+    'data/levels/gta5/snowheavy_render_drop.xml',
+    'data/levels/gta5/snowheavy_render_ground.xml',
+    'data/levels/gta5/time.xml',
+    'data/levels/gta5/urban_dust_render_drop.xml',
+    'data/levels/gta5/water.xml',
+    'data/levels/gta5/weather.xml',
+    'data/levels/gta5/woodland_mist_render_ground.xml',
+    'data/timecycle/w_clear.xml',
+    'data/timecycle/w_clearing.xml',
+    'data/timecycle/w_extrasunny.xml',
+    'data/timecycle/w_foggy.xml',
+    'data/timecycle/w_overcast.xml',
+    'data/timecycle/w_rain.xml',
+    'data/timecycle/w_thunder.xml',
+}
+
+client_script 'client.lua'
+
+
+this_is_a_map 'yes'
